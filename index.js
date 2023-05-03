8e26a465f862d165c624f01bbe67c234dd63ce70
BaurMus <musilimov_b@mail.ru>
2023-05-03 16:41:16 +0600

    node demo module path

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-//(function(exports, require, module, __dirname, __filename) {
 const chalk = require('chalk');
 const text = require('./data.js'); // or ./data
 
 console.log(chalk.blue(text));
 
-//console.log(__dirname);
 console.log(__filename);
-//})
